5a0a1ba56f57bcf3fcfd1c59689ba8e8113fde2b
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-15 03:55:28 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Auth: Kassahun Shiemlis
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints words.
+ *
+ * Return: returns 0.
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
+}
