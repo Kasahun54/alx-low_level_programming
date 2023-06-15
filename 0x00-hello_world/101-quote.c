3d9b69c3c99539a23b8138b09529aed2eeefebb3
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-15 05:37:29 -0700

    print the word with out function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*
+ * File: 101-quote.c
+ * Auth: Kassahun Shiemlis
+ */
+
+#include <stdio.h>
+
+/**
+ * main - printing words.
+ *
+ * Return: returns 1.
+ */
+int main(void)
+{
+string haha ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+return (haha);
+}
