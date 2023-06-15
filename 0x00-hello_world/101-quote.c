df4014bd5ac76ef19cb655b1f77cf00c04bc8d18
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-15 05:59:03 -0700

    print the word with out function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  */
 
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - printing words.
  *
@@ -12,6 +12,6 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
