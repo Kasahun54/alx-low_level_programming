045514c552a00b85aadccedd9e7613953ef16f80
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-16 05:38:58 -0700

    print alphabet with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int n = 0 ;
+
+	while (n < 26)
+	{
+		if(a != 'q' || a != 'e')
+		{
+			putchat(a);
+		}
+		a++;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
