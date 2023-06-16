2fed1292b0edaa7a7598cf75b1ade622e4394c03
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-16 06:06:36 -0700

    print numbers 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers between 0-9 *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return 0;
+}
