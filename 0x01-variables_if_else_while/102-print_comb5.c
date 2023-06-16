51e30dd8e069e9c7d655d5028713a85796bf925e
Kassahun Shimelis <133364928+Kasahun54@users.noreply.github.com>
2023-06-17 00:30:47 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Program entry point
+*
+* Return: 0 on success. Error code otherwise
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+if (i != j)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 99 || j != 98);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
