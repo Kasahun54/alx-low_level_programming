f46227019c51ce281deb45b3ad86db4370b8a6f7
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-16 05:26:53 -0700

    print capital and small letter alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio>
+/**
+ * main - print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s = 'a';
+	char c = 'A';
+	int n = 0;
+	while (n < 52)
+	{
+		if (n < 26)
+		{
+		putchar(s);
+		s++;
+		}
+		else
+		{
+		putchar(c);
+		c++;
+		}
+	}
+	n++;
+	putchar('\n');
+	return (0);
+}
