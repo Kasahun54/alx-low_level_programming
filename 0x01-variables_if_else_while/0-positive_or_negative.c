45509257ed6e995002b7250f613c42d06a02516f
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-16 04:14:52 -0700

    print eithrt positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main : Enrty point 
+ * main : Enrty point
  *
  * Return : the number is pos or neg or zero
  */
@@ -11,17 +11,17 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if (n = 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 		printf("%d is negative\n", n);
 	}
