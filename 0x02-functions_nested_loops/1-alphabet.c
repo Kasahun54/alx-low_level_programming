a251ce54e7638a89ec9f6cc3f69754384f981dc8
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-20 10:42:43 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  main - print alphabet
+ *
+ *  return: return small letter
+ */
+void print_alphabet(void)
+{
+	int i;
+	for(i = 97; i<=122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
