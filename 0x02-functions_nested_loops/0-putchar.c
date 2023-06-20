9783352d0cbad3a728cf0b64a82aaf8e598f22c4
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-20 10:16:14 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-#include "0-putchar.c"
 #include "main.h"
 
-/**
- * main : strating point
- *
- * return : return value
- */
-int main 
+/** main : Entry point
+ * Return : Always return 0 (Success.)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
+}
