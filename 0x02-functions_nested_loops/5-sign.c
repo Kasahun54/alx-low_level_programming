7d548c1c0ef8ec77a5ea922b9277920695b5bd4f
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-20 12:21:22 -0700

    print a number sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * 
  * @n: decimal number
  *
- * Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
