0d9d277dc18dbbd7370c21637c7b949696bb7bd0
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-22 10:17:23 -0700

    drow straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(95);
+	}
+}
