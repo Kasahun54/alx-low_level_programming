fc2b4df4f05c15d053c02cafbd1812d55fb26b98
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-22 04:10:42 -0700

    multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplay two.
+ * @a: input number.
+ * @b: input number.
+ * Return: the result of the mul always success.
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
