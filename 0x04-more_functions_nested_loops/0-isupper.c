b78650ba023255e00afe3588e0c6fada2c193505
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-22 03:22:40 -0700

    check for uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case latter
+ *
+ * return 1 if the letter is capital else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	else 
+	{
+		return (0);
+	}
+}
