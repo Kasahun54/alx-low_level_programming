98cd4297c6ca015db8826ae9bffab06ba0d77369
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 05:35:40 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy n strings
+ * @dest: destination
+ * @src: Source
+ * @n: number of string to copy
+ * return: return to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c1 = 0, c2 = 0;
+
+	while (c2 < n)
+	{
+		*(dest + c1) = *(src + c2);
+		if (*(src + c2) == '\0')
+			break;
+		c1++;
+		c2++;
+	}
+	return (dest);
+}
