2dff43b75229fed6a51783d9ceaba2b8c5cc593d
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 06:14:01 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c1 = 0, c2 = 0;
 
-	while (c2 < n)
+	while (c2 < n && src[c2] != '\0')
 	{
 		*(dest + c1) = *(src + c2);
 		if (*(src + c2) == '\0')
