a939be898150393218e484f9654e27ad2b83e64d
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 09:26:03 -0700

    compare the charachter

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two string
+ * @s1: string one
+ * @s2: string two
+ * return: return the differnce with ASCII
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, result;
+
+	for  (i = 0; *s1 != '\0'; i++)
+	{
+		result = *(s1 + i) - *(s2 + i);
+	if (result == 0)
+		continue;
+	else
+		return result;
+	}
+	return result;
+}
