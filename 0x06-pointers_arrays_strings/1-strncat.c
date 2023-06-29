ff77076b4f02808c3b4d62567ee203face2f6a6c
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 03:18:56 -0700

    concatinate nth location string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int c1 = 0, c2 = 0;
 
-	while(*(dest + c1) != '\0')
+	while (*(dest + c1) != '\0')
 	{
 		c1++;
 	}
 
-	while(c2 < n)
+	while (c2 < n)
 	{
 		*(dest + c1) = *(src + c2);
 		if (*(src + c2) == '\0')
