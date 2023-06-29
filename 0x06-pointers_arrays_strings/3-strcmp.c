620a63b6a400af85c716f7874b41fd2ade41fd9d
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 10:02:05 -0700

    compare the charachter

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * _strcmp - compare two strings
- * @s1: string one.
- * @s2: string two.
- * return: return 0 if the same differnce with ASCII
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i, result = 0;
 
-	for(i = 0; result == 0; i++)
+	for (i = 0; result == 0; i++)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
