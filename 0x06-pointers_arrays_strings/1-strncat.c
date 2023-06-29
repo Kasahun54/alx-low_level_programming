5309407cee60807cd4ee9008fe627bb943f6be0c
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 02:59:17 -0700

    concatinate with n character

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c1 = 0, c2 = n ;
+	int c1 = 0, c2 = 0 ;
 
 	while(*(dest + c1) != '\0')
 	{
 		c1++;
 	}
 
-	while(c2 >= 0)
+	while(c2 < n)
 	{
 		*(dest + c1) = *(src + c2);
 		if (*(src + c2) == '\0')
