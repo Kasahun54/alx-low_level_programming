1b3660307bc5d1ac07eba738c71e26da1f30b201
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-06-29 03:11:22 -0700

    concatinate with n character

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,11 @@
  * @dest: destination.
  * @src: source.
  * @n: amount of byte.
- * Return : Pointer to dest.
+ * Return: Pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c1 = 0, c2 = 0 ;
+	int c1 = 0, c2 = 0;
 
 	while(*(dest + c1) != '\0')
 	{
