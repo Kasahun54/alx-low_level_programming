93602ad261663dbb4eed95e4d8ac1683191d8b08
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-05 10:18:05 -0700

    print string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
+
 /**
- * _strlen_recursion - print string length
- * @s: String
- * return: the length of a string
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
