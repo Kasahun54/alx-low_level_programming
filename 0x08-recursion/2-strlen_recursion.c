982bc3c01605471b81139096750509812200fc52
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-05 10:01:13 -0700

    print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print string length
+ * @s: String
+ * return: return length
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
