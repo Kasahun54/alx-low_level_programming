c3e4181335544b8638ca8e4bc23c9dd377dddda7
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-07 09:29:32 -0700

    print number

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,21 +3,13 @@
  * main - print all arguments
  * @argc: number of command
  * @argv: array contain the program comand
- * Return: 0 success
+ * Return: 0 - success
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
-	else
-	{
-		printf("%s\n", argv[argc]);
-	}
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
