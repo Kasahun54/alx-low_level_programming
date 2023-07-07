283400af064ea6eb76cb4c784ff95ed4d91834a9
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-07 11:32:01 -0700

    ststic

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * main - put string
+ * Return: 0 success
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
