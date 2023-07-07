80b473f6e6f5ea83112405d066a34bb696331812
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-07 09:27:14 -0700

    print number

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - display the string
- * @argc: number
- * @argv: array
+ * main - print all arguments
+ * @argc: number of command
+ * @argv: array contain the program comand
  * Return: 0 success
  */
 int main(int argc, char *argv[])
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	int i;
 	if (argc > 1)
 	{
-		for (i = 0; i > argc; i++)
+		for (i = 0; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
 		}
