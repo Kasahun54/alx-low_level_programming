ea3019ff00ef1d6530fb0677072a0083e2555311
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-07 01:35:59 -0700

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - display the file name
+ * @argc: number grater than 1
+ * @argv: string that hold
+ * return: return a file name
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc]);
+}
