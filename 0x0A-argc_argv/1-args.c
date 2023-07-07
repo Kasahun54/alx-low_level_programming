46f0aec65be2fe691fb51c2626380f74332bdd63
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-07 01:42:06 -0700

    display the number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - display the argc number
+ * @argc: number
+ * @argv: array contain string
+ * return: return 0 always success
+ */
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
