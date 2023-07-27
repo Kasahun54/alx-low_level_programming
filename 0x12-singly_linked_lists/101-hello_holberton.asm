904c3f11a239b6ab125c7827a08082b3dd4c9168
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-27 12:27:13 -0700

    assembly lang

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s", 0
+
+section .text
+	extern printf
+
+global main
+main:
+	sub rsp, 8
+	mov rdi, format
+	mov rsi, hello
+	xor eax, eax
+	call printf
+	add rsp, 8
+
+	mov eax, 0x60
+	xor edi, edi
+	syscall
