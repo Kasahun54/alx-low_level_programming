8cd39b4c7d5254721058fb49be28683e1300a9f2
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-07-27 12:18:10 -0700

    excute before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - excute before the main function
+ * return: always success
+ */
+void before_main()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
