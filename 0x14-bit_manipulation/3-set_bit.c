7bc63f7f61cb95ebe5210aab600d6072bde0dfae
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-08-03 11:27:16 -0700

    set value

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,13 +5,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
+	if (index > 32)
 		return (-1);
-	}
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
-	
-	return (1);
-
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
 }
