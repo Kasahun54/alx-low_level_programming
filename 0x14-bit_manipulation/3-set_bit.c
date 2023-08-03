7c5bcfcacc6c316952e135a31fd0c9194b9a59c5
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-08-03 12:00:46 -0700

    get bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,9 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-
-	return (-1);
+	*n = ((1UL << index) | *n);
+	return (1);
 }
