1be59b1439f794f3acccdd28d35eacd21b4d9777
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-08-03 10:59:25 -0700

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_binary - convert decimal to binary
+ * @n: number in base ten
+ * Return: the binary 
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) ? '1' : '0');
+}
