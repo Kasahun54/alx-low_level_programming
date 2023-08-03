6a2ff7333d6692ccf252436a4c3a0b9f8ff33946
kassahun shimelis <kassahunshimelis54@gmail.com>
2023-08-03 11:34:24 -0700

    set value

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,12 +8,12 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
+	}
+	unsigned long int mask = ~(1UL << index);
+	*n &= mask;
 
-	(*n) &= ~(1 << index);
-
-	if (get_bit((*n), index) == 0)
-		return (1);
-	return (-1);
+	return (1);
 }
