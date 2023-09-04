e66e762204ea4d66f32b422aeac6500588b25daf
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-09-04 10:02:12 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[len])
+		while (text_content[length])
 			length++;
 		wr = write(fd, text_content, length);
 		if (wr == -1)
