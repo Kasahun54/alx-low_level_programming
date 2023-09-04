5cd530997118b0038ce85ae913e9917fd312261b
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-09-04 10:16:40 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,22 +14,19 @@ int main(int ac, char **av)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
-	fdFrom = open(av[1], O_RDONLY);
+	}	fdFrom = open(av[1], O_RDONLY);
 	if (fdFrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
-	fdTo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	}	fdTo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	while ((reads = read(fdFrom, buff, 1024)) > 0)
-	{
-		wr = write(fdTo, buff, reads);
+	{	wr = write(fdTo, buff, reads);
 		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -37,18 +34,15 @@ int main(int ac, char **av)
 		}
 	}
 	if (reads == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (close(fdFrom) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrom);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrom);
 		exit(100);
 	}
 	if (close(fdTo) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdTo);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdTo);
 		exit(100);
 	}
 	return (0);
