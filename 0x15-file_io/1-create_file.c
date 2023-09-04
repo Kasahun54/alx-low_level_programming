4817c149e15c4d1c440e81de8418cc0d06b9c776
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-09-04 09:59:57 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - function to create a file
+ * @filename: name of the file
+ * @text_content: create an empty file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length = 0;
+	long int wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			length++;
+		wr = write(fd, text_content, length);
+		if (wr == -1)
+			return (-1);
+	}
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+}
