00d7ae6a5082bcbb7d7cc3d6c6f7e6fc6e45d3bf
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-12 09:20:31 -0700

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-  * print_dlistint - print integers in the doubly linked list.
-  * @h: point to the first node.
-  * 
-  * Return: counting num of nodes.
-  */
+ * print_dlistint - print integers in the doubly linked list.
+ * @h: point to the first node.
+ *
+ * Return: counting num of nodes.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *tmp;
