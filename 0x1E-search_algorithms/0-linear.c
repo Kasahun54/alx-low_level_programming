7455b64b8c3990996b93c47a978a23748fd4aea0
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2024-02-07 21:58:45 +0300

    searching algoritem

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - Search the value in an array
+  * @array: A pointer to index
+  * @size: The number of elements
+  * @value: search value
+  *
+  * Return: If the value is empty -1. else display index
+  *
+  * Description: Prints a value every time.
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
